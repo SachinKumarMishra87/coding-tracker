ed1f25602847d83e479a63c36024e5d2dff371ae
SachinKumarMishra87 <sachinmishra8707@gmail.com>
2026-06-12 00:58:58 +0530

    fix google oauth callback url

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -18,8 +18,7 @@ passport.use(
       clientSecret:
         process.env.GOOGLE_CLIENT_SECRET,
 
-      callbackURL:
-        "/api/auth/google/callback"
+      callbackURL: process.env.GOOGLE_CALLBACK_URL
 
     },
 
