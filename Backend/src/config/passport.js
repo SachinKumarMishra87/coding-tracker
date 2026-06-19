99c01a382c20df29099c8d97a8865a9b8cab6863
SachinKumarMishra87 <sachinmishra8707@gmail.com>
2026-06-19 23:47:12 +0530

    fix: passport proxy true added for custom domain

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -1,78 +1,38 @@
 import passport from "passport";
-
-import {
-  Strategy as GoogleStrategy
-} from "passport-google-oauth20";
-
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../model/User.js";
 
 passport.use(
-
   new GoogleStrategy(
-
     {
-
-      clientID:
-        process.env.GOOGLE_CLIENT_ID,
-
-      clientSecret:
-        process.env.GOOGLE_CLIENT_SECRET,
-
-      callbackURL: process.env.GOOGLE_CALLBACK_URL
-
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      proxy: true // 🎯 CRITICAL: Custom domain (api.leetpattracker.in) ko target karne ke liye proxy allow karna zaroori hai
     },
-
-    async (
-      accessToken,
-      refreshToken,
-      profile,
-      done
-    ) => {
-
+    async (accessToken, refreshToken, profile, done) => {
       try {
-
         let user = await User.findOne({
-
-          email:
-            profile.emails[0].value
-
+          email: profile.emails[0].value
         });
 
         // IF USER NOT EXISTS
-
         if (!user) {
-
           user = await User.create({
-
-            username:
-              profile.displayName,
-
-            email:
-              profile.emails[0].value,
-
-            googleId:
-              profile.id,
-
+            username: profile.displayName,
+            email: profile.emails[0].value,
+            googleId: profile.id,
             isVerified: true,
-
             password: "google-login"
-
           });
-
         }
 
         return done(null, user);
-
       } catch (error) {
-
         return done(error, null);
-
       }
-
     }
-
   )
-
 );
 
 export default passport;
