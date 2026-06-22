b3a508714dca0235820aeb183dafca35255df7dd
SachinKumarMishra87 <sachinmishra8707@gmail.com>
2026-06-22 23:35:27 +0530

    e

--- Backend/src/config/mail.js
+++ Backend/src/config/mail.js
@@ -3,24 +3,17 @@ import nodemailer from "nodemailer";
 
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // 587 ke liye false
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
-
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS available:", !!process.env.EMAIL_PASS);
-
-transporter.verify(function (error, success) {
-    if (error) {
-        console.log("SMTP Error:", error);
-    } else {
-        console.log("SMTP Server is ready");
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 export default transporter;
